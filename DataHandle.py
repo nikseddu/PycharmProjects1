02b06e0a485f6fa16188915279989d049fba7ea9
nikseddu <nikcena007@gmail.com>
2021-06-15 15:11:39 +0530

    Update DataHandle.py

--- DataHandle.py
+++ DataHandle.py
@@ -17,59 +17,29 @@ for ifile_name in os.listdir(Data_dir):
             print(file_path)
             final_data.extend(json_data)
 
-print(final_data[0])
+    else:
+        pass
 
+print(len(final_data))
+#
 
-annotation_result = final_data[0]['annotations'][0]['result']
 total_list=[]
+for j in  range(len(final_data)):
+    annotation_result = final_data[j]['annotations'][0]['result']
 
-for i in annotation_result:
-    entity_list = []
-    # print(i['value'], "\n")
-    values = i['value']
-    # print(values["text"])
-    #
-    # print(i['text'], "\n")
-    entity_list.append([values['start'],values['end'],values['labels'][0]])
-    total_list.append((values['text'], {'entities': entity_list}))
-#     # print("\n")
+
+    for i in annotation_result:
+        entity_list = []
+        # print(i['value'], "\n")
+        values = i['value']
+        # print(values["text"])
+        #
+        # print(i['text'], "\n")
+        entity_list.append([values['start'],values['end'],values['labels'][0]])
+        total_list.append((values['text'], {'entities': entity_list}))
+    #     # print("\n")
 
 print(total_list)
-
-
-
-
-
-
-
-
-# data1 = data[0]
-#
-# # print(data1["annotations"])
-#
-# training_data = []
-#
-
-# annotation_result = data1['annotations'][0]['result']
-
-# print(annotation_result)
-
-
-
-
-
-
-# for r in annotation_result:
-#
-#         r.pop('id')
-#         # r.pop('from_name')
-#         # r.pop('to_name')
-#         # r.pop('type')
-
-# print(annotation_result)
-
-# annotation_result["value"]
-
 
 
 
@@ -78,7 +48,6 @@ print(total_list)
 # train =[("Money Transfer from my checking account is not working",{"entities":[(6,13,"ACTIVITY"),(23,39,"PRODUCT")]}),
 #         ("I want to check balance in saving account",{"entities":[(16,23,"ACTIVITY"),(30,45,"PRODUCT")]})
 # ]
-
 
 
 # with open("Data/194.json", "r") as f:
